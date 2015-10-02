8c1b9ef7bddddb530b937ab30356334e822064cb
fans656 <fans656@yahoo.com>
2015-10-02 17:37:03 +0800

    将业务逻辑从flask框架代码中移出

--- f6helloworld/1/main.py
+++ f6helloworld/1/main.py
@@ -1,86 +1,17 @@
 # coding: utf-8
-from datetime import datetime, timedelta
+from flask import Flask
 
-from flask import Flask, jsonify, request
-
-from createdb import cur
+from routes import routes
 
 app = Flask('server')
-
-def to_array(d):
-    return [t.values()[0] for t in d]
 
 @app.route('/')
 def root():
     return app.send_static_file('index.html')
 
-@app.route('/test')
-def test():
-    return jsonify({'hello': 'world'})
-
-@app.route('/buildings')
-def buildings():
-    cur.execute('select distinct building from rooms')
-    r = cur.fetchall()
-    return jsonify({'buildings': to_array(r)})
-
-@app.route('/floors')
-def floors():
-    try:
-        building = request.args.get('building').encode('utf-8')
-    except AttributeError:
-        return error('Require <building> to be specified.')
-    cur.execute('select distinct floor from rooms where building = %s', (building,))
-    return jsonify({
-        'building': building,
-        'floors': to_array(cur.fetchall())})
-
-@app.route('/rooms')
-def rooms():
-    try:
-        building = request.args.get('building').encode('utf-8')
-        floor = request.args.get('floor').encode('utf-8')
-    except AttributeError:
-        return error('Require <building> and <floor> to be specified.')
-    cur.execute('select room from rooms where building = %s and floor = %s', (building, floor))
-    return jsonify({
-        'building': building,
-        'floor': floor,
-        'rooms': to_array(cur.fetchall())
-        })
-
-@app.route('/lessons')
-def lessons():
-    try:
-        building = request.args.get('building').encode('utf-8')
-        room = request.args.get('room')
-        date = request.args.get('date')
-    except AttributeError:
-        return error('Require <building> and <room> and <date> to be specified.')
-    room = int(room)
-    date = datetime.strptime(date, '%Y-%m-%d')
-    weekday = date.isoweekday()
-    cur.execute('select * from lessons where building = %s and room = %s and weekday = %s',
-            (building, room, weekday))
-    r = cur.fetchall()
-    for t in r:
-        t['date'] = date.strftime('%Y-%m-%d')
-        del t['weekday']
-    return jsonify({'lessons': r})
-
-@app.route('/rooms-of-lesson')
-def rooms_of_lesson():
-    lesson = request.args.get('lesson').encode('utf-8')
-    date = request.args.get('date')
-    weekday = datetime.strptime(date, '%Y-%m-%d').isoweekday()
-    cur.execute('select building, room, teacher, beg, end from lessons where lesson = %s and weekday = %s',
-            (lesson, weekday))
-    r = cur.fetchall()
-    print r
-    return jsonify({'rooms': r, 'lesson': lesson})
-
-def error(msg):
-    return jsonify({'error': msg})
+for url, v in routes.items():
+    func = v['func']
+    app.route(url)(func)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=6560, debug=True)
---
+++ f6helloworld/1/routes.py
@@ -0,0 +1,115 @@
+# coding: utf-8
+from datetime import datetime, timedelta
+
+from flask import jsonify, request
+
+from createdb import cur
+
+def to_array(d):
+    return [t.values()[0] for t in d]
+
+def error(msg):
+    return jsonify({'error': msg})
+
+def buildings():
+    cur.execute('select distinct building from rooms')
+    r = cur.fetchall()
+    return jsonify({'buildings': to_array(r)})
+
+def floors():
+    try:
+        building = request.args.get('building').encode('utf-8')
+    except AttributeError:
+        return error('Require <building> to be specified.')
+    cur.execute('select distinct floor from rooms where building = %s', (building,))
+    return jsonify({
+        'building': building,
+        'floors': to_array(cur.fetchall())})
+
+def rooms():
+    try:
+        building = request.args.get('building').encode('utf-8')
+        floor = request.args.get('floor').encode('utf-8')
+    except AttributeError:
+        return error('Require <building> and <floor> to be specified.')
+    cur.execute('select room from rooms where building = %s and floor = %s', (building, floor))
+    return jsonify({
+        'building': building,
+        'floor': floor,
+        'rooms': to_array(cur.fetchall())
+        })
+
+def lessons():
+    try:
+        building = request.args.get('building').encode('utf-8')
+        room = request.args.get('room')
+        date = request.args.get('date')
+    except AttributeError:
+        return error('Require <building> and <room> and <date> to be specified.')
+    room = int(room)
+    date = datetime.strptime(date, '%Y-%m-%d')
+    weekday = date.isoweekday()
+    cur.execute('select * from lessons where building = %s and room = %s and weekday = %s',
+            (building, room, weekday))
+    r = cur.fetchall()
+    for t in r:
+        t['date'] = date.strftime('%Y-%m-%d')
+        del t['weekday']
+    return jsonify({'lessons': r})
+
+def rooms_of_lesson():
+    lesson = request.args.get('lesson').encode('utf-8')
+    date = request.args.get('date')
+    weekday = datetime.strptime(date, '%Y-%m-%d').isoweekday()
+    cur.execute('select building, room, teacher, beg, end from lessons where lesson = %s and weekday = %s',
+            (lesson, weekday))
+    r = cur.fetchall()
+    print r
+    return jsonify({'rooms': r, 'lesson': lesson})
+
+routes = {
+    '/buildings': {
+        'func': buildings,
+        'doc': {
+            'desc': u'返回所有教学楼名称',
+            'params': u'无',
+            'return': u''''''
+        }
+    },
+
+    '/floors': {
+        'func': floors,
+        'doc': {
+            'desc': u'',
+            'params': u'',
+            'return': u''
+        }
+    },
+
+    '/rooms': {
+        'func': rooms,
+        'doc': {
+            'desc': u'',
+            'params': u'',
+            'return': u''
+        }
+    },
+
+    '/lessons': {
+        'func': lessons,
+        'doc': {
+            'desc': u'',
+            'params': u'',
+            'return': u''
+        }
+    },
+
+    '/rooms-of-lesson': {
+        'func': rooms_of_lesson,
+        'doc': {
+            'desc': u'',
+            'params': u'',
+            'return': u''
+        }
+    }
+}
